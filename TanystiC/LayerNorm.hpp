ad2e36dde32b58f427daefccd0aa6af1485ba2da
EnaAlogo <93442086+EnaAlogo@users.noreply.github.com>
2023-02-13 18:44:38 +0200

    Fixed layer norm
    fixed the part where layer norm was quite literally just not doing layer norm(i think)

--- TanystiC/LayerNorm.hpp
+++ TanystiC/LayerNorm.hpp
@@ -9,9 +9,7 @@ class LayerNorm : public Layer<T>
 	using Tensor = Layer<T>::Tensor;
 	using Vector = Layer<T>::Vector;
 public:
-
-	LayerNorm(
-		const f64 momentum = .99,
+LayerNorm(
 		const f64 eps = 1e-5,
 		const bool center = true,
 		const bool scale = true,
@@ -29,13 +27,21 @@ public:
 		if (!built)
 			build(inputs);
 
-		Tensor mean = reduce::mean(inputs, { -1 });
-		Tensor var = reduce::variance(inputs, { -1 });
+		Tensor mean = reduce::mean(inputs, { -1 } , 1);
+		Tensor var = reduce::variance(inputs, { -1 } , 1);
 		if (training) {
-			std = reduce::stddev(inputs, { -1 });
+			std = reduce::stddev(inputs, { -1 },1);
 			x_norm = ops::Divde(ops::Subtract(inputs, mean), std);
 		}
-		return nn::batch_norm(inputs, mean, var, gamma, beta, eps);
+
+		smallvec<size_t> bcastshape(inputs.rank());
+		for (size_t& i : bcastshape) i = 1;
+		bcastshape[-1] = inputs.shape()[-1];
+
+		Tensor scale = ops::broadcast_to(gamma, bcastshape);
+		Tensor offset = ops::broadcast_to(beta, bcastshape);
+
+		return nn::batch_norm(inputs, mean, var, scale, offset, eps);
 
 	}
 
@@ -58,7 +64,7 @@ public:
 	Tensor backwards(const Tensor& out_grad, f64 lr)
 	{
 
-		auto [dx, db, dg] = grad::layer_norm_backwards(out_grad, x_norm, std, gamma);
+		auto [dx, db, dg] = grad::batch_norm_backwards(out_grad, x_norm, std, gamma);
 
 		nn::rm(gamma *= lr, dg);
 		nn::rm(beta *= lr, db);
